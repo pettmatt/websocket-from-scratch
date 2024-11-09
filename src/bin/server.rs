d9d0257fd0ed1a2f40a9102e649219d1d9acceec
pettmatt <petterimattila@protonmail.com>
2024-11-09 15:17:32 +0200

    Routing added

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,17 +1,52 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::future::Future;
 
-use http_body_util::Full;
-use hyper::body::Bytes;
+use http_body_util::{Full, BodyExt, combinators::BoxBody};
+use hyper::body::{Bytes, Frame};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Request, Response};
+use hyper::{Request, Response, Method, StatusCode};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
 // Service that return simple 200 response.
 async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
+
+async fn routing(
+    request: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (request.method(), request.uri().path()) {
+        (&Method::GET, "/") => {
+            Ok(Response::new(full(
+                "Try POSTing data to /websocket",
+            )))
+        },
+        (&Method::GET, "/websocket") => {
+            Ok(Response::new(request.into_body().boxed()))
+        },
+        // Return 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+// Utility functions for routing
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    http_body_util::Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
 
 #[tokio::main]
@@ -25,17 +60,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     loop {
         let (stream, _) = listener.accept().await?;
 
+        println!("Stream {:?}", stream);
+        
         // Use an adapter to access something implementing `tokio::io` traits as if they implement
         // `hyper::rt` IO traits.
         let io = TokioIo::new(stream);
 
+        println!("IO {:?}", io);
+        
         // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
             // Finally, we bind the incoming connection to our `hello` service
             let builder = http1::Builder::new();
             if let Err(error) = builder
                 // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(hello))
+                .serve_connection(io, service_fn(routing))
                 .await
             {
                 eprintln!("Error serving connection: {:?}", error);
