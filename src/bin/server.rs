5e8705e96caf22ed2c9096982e5c61b4047628a8
pettmatt <petterimattila@protonmail.com>
2024-12-04 17:05:06 +0200

    Separating match arm into two so the server can return better errors + timer

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,6 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use std::future::Future;
-use std::u16;
+use std::time;
 
 use http_body_util::{Full, BodyExt, combinators::BoxBody};
 use hyper::body::{Bytes, Frame};
@@ -16,44 +15,50 @@ async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
 
-async fn routing(
-    request: Request<hyper::body::Incoming>,
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-    match (request.method(), request.uri().path()) {
-        (&Method::GET, "/") => {
-            Ok(Response::new(full(
-                "Try to request /websocket",
-            )))
-        },
-        (&Method::GET, "/websocket") => {
-            let upgrade_header = request.headers().get("Upgrade")
-                .and_then(|header| header.to_str().ok())
-                .unwrap_or("Missing Upgrade value");
-
-            if upgrade_header != "websocket" {
-                let error_response = error(
-                    StatusCode::NOT_ACCEPTABLE,
-                    String::from("Unacceptable Upgrade value")
-                );
-                return Ok(error_response);
-            }
-
-            let (parts, _body) = request.into_parts();
-            let message = parts.headers
-                .iter()
-                .map(|(key, value)| {
-                    let key_str = key.as_str();
-                    let value_str = value.to_str().unwrap_or_default();
-                    format!("{}: {}", key_str, value_str)
+async fn routing(request: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match request.method() {
+        &Method::GET => {
+            match request.uri().path() {
+                "/" => {
+                    Ok(Response::new(full(
+                        "Try to request /websocket",
+                    )))
+                },
+                "/websocket" => {
+                    let upgrade_header = request.headers().get("Upgrade")
+                        .and_then(|header| header.to_str().ok())
+                        .unwrap_or("Missing Upgrade value");
+        
+                    if upgrade_header != "websocket" {
+                        let error_response = error(
+                            StatusCode::NOT_ACCEPTABLE,
+                            String::from("Unacceptable Upgrade value")
+                        );
+                        return Ok(error_response);
+                    }
+        
+                    let (parts, _body) = request.into_parts();
+                    let message = parts.headers
+                        .iter()
+                        .map(|(key, value)| {
+                            let key_str = key.as_str();
+                            let value_str = value.to_str().unwrap_or_default();
+                            format!("{}: {}", key_str, value_str)
+                        }
+                    ).collect::<Vec<_>>().join("\n");
+        
+                    Ok(Response::new(full(message)))
+                },
+                _ => {
+                    let mut not_found = Response::new(empty());
+                    *not_found.status_mut() = StatusCode::NOT_FOUND;
+                    Ok(not_found)
                 }
-            ).collect::<Vec<_>>().join("\n");
-
-            Ok(Response::new(full(message)))
+            }
         },
-        // Return 404 Not Found for other routes.
         _ => {
             let mut not_found = Response::new(empty());
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            *not_found.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
             Ok(not_found)
         }
     }
@@ -80,9 +85,6 @@ fn error<T: Into<Bytes>>(status_code: StatusCode, message: T) -> Response<BoxBod
         .expect("Failed to process request")
 }
 
-// fn error<T: Into<Bytes>>(status: StatusCode, message: T) -> Response<BoxBody<Bytes, Infallible>> {
-// }
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let address = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -94,15 +96,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     loop {
         let (stream, _) = listener.accept().await?;
 
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement `hyper::rt` IO traits.
         let io = TokioIo::new(stream);
 
         // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
+            let start = time::SystemTime::now();
             let builder = http1::Builder::new();
+            // `service_fn` converts our function to a `Service`
             if let Err(error) = builder
-                // `service_fn` converts our function in a `Service`
                 .serve_connection(io, service_fn(routing))
                 .await
             {
@@ -110,6 +112,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             } else {
                 println!("Serving connection");
             }
+
+            println!("Time used to process {:?}", start.elapsed().unwrap_or_default());
         });
     }
 }
